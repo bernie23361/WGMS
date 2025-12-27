e7b40d82925085dd88911b805429cf712d8d0634
bernie23361 <41538038+bernie23361@users.noreply.github.com>
2025-12-27 10:40:04 +0800

    Update login_ui.py

--- login_ui.py
+++ login_ui.py
@@ -3,16 +3,10 @@ import gradio as gr
 def create_login_ui():
     gr.Markdown("# WGMS 氣象地動監測平台")
     with gr.Column(elem_classes="login-card"):
-        # 對應您 XD 裡的帳號密碼框
-        user = gr.Textbox(label="用戶名", placeholder="請輸入用戶名")
+        user = gr.Textbox(label="帳號", placeholder="請輸入帳號")
         pw = gr.Textbox(label="密碼", type="password", placeholder="請輸入密碼")
-        
-        # 蘇老闆設計的「記住密碼」
         remember = gr.Checkbox(label="記住密碼", value=False)
-        
-        login_btn = gr.Button("登錄", elem_classes="login-btn")
+        login_btn = gr.Button("進入系統", elem_classes="login-btn")
         error_msg = gr.Markdown("", visible=False)
-        
-        gr.Markdown("天氣概況Weather工作室版權所有．", elem_id="copyright")
-        
+        gr.Markdown("天氣概況Weather工作室版權所有．")
     return {"user": user, "pass": pw, "btn": login_btn, "error": error_msg}
